aed712d4cb6e036e5f8a9066abcba0acdee12484
Jovi Simons <jovisimons009@gmail.com>
2021-11-23 09:50:13 +0100

    added categories unit tests

--- src/main/kotlin/com/example/menuservice/controller/CategoryController.kt
+++ src/main/kotlin/com/example/menuservice/controller/CategoryController.kt
@@ -2,50 +2,43 @@ package com.example.menuservice.controller
 
 import com.example.menuservice.model.Category
 import com.example.menuservice.repo.CategoryRepo
+import com.example.menuservice.service.CategoryService
+import com.example.menuservice.service.ProductService
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
-internal class CategoryController(repository: CategoryRepo) {
-    private val repo: CategoryRepo
-
+internal class CategoryController(service: CategoryService) {
+    private val service: CategoryService
 
     @GetMapping("/categories")
     fun all(): List<Category> {
-        return repo.findAll()
+        return service.categories
     }
 
     @PostMapping("/categories")
     fun newCategory(@RequestBody newCategory: Category): Category {
-        return repo.save(newCategory)
+        return service.saveCategory(newCategory)!!
     }
 
     // Single item
     @GetMapping("/categories/{id}")
-    fun one(@PathVariable id: Int): Optional<Category> {
-        return repo.findById(id)
+    fun one(@PathVariable id: Int): Category? {
+        return service.getCategoryById(id)
     }
 
     @PutMapping("/categories/{id}")
     fun replaceCategory(@RequestBody newCategory: Category, @PathVariable id: Int): Category {
-        return repo.findById(id)
-                .map { category ->
-                    category.name = newCategory.name
-                    category.description = newCategory.description
-                    repo.save(category)
-                }
-                .orElseGet {
-                    repo.save(newCategory)
-                }
+        return service.updateCategory(newCategory)
     }
 
     @DeleteMapping("/categories/{id}")
     fun deleteCategory(@PathVariable id: Int) {
-        repo.deleteById(id)
+        service.deleteCategory(id)
     }
 
     init {
-        this.repo = repository
+        this.service = service
     }
 }
