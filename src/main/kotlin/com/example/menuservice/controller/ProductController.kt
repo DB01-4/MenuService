c9b073edfa10b1d44e0b6c590912c7538c918bd3
Semm6 <sem.dewilde6@gmail.com>
2021-12-16 21:36:42 +0100

    Changed endpoints

--- src/main/kotlin/com/example/menuservice/controller/ProductController.kt
+++ src/main/kotlin/com/example/menuservice/controller/ProductController.kt
@@ -5,7 +5,7 @@ import com.example.menuservice.service.ProductService
 import org.springframework.web.bind.annotation.*
 
 
-@RequestMapping(path = ["api/private"])
+@RequestMapping(path = ["api/"])
 @CrossOrigin(origins = ["*"])
 @RestController
 internal class ProductController(service: ProductService) {
@@ -13,32 +13,32 @@ internal class ProductController(service: ProductService) {
     private val service: ProductService
 
 
-    @GetMapping("/products")
+    @GetMapping("public/products")
     fun all(): List<Product> {
         return service.products
     }
 
-    @GetMapping("/products/category/{id}")
+    @GetMapping("public/products/category/{id}")
     fun getByCategoryId(@PathVariable id: Int): List<Product> {
         return service.findByCategoryId(id)
     }
 
-    @PostMapping("/products")
+    @PostMapping("private/products")
     fun newProduct(@RequestBody newProduct: Product): Product {
         return service.saveProduct(newProduct)
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("public/products/{id}")
     fun one(@PathVariable id: Int): Product? {
         return service.getProductById(id)
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("private/products/{id}")
     fun replaceProduct(@RequestBody newProduct: Product, @PathVariable id: Int): Product {
         return service.updateProduct(newProduct)
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("private/products/{id}")
     fun deleteProduct(@PathVariable id: Int) {
         service.deleteProduct(id)
     }
