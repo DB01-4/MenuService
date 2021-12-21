f779af25dd510edec71dad7a58b276fd0b0fc969
Koen Evers <470340@student.fontys.nl>
2021-12-21 12:52:50 +0100

    Update Order.kt
    added ordertime

--- src/main/kotlin/com/example/menuservice/model/Order.kt
+++ src/main/kotlin/com/example/menuservice/model/Order.kt
@@ -2,6 +2,7 @@ package com.example.menuservice.model
 
 import javax.persistence.*
 import com.example.menuservice.model.OrderItem
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "orders")
@@ -9,19 +10,22 @@ class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Int? = null;
+    var id: Int? = null
 
     @Column(name = "table_id")
-    var tableId: Int? = null;
+    var tableId: Int? = null
+
+    @Column(name = "order_time", columnDefinition = "TIMESTAMP" )
+    var orderTime: LocalDateTime? = null
 
     @Column(name = "status", nullable = true)
-    var status: Int? = null;
+    var status: Int? = null
 
     @OneToMany(cascade = [CascadeType.ALL] ,orphanRemoval = true)
     @JoinColumn(name = "order_id", referencedColumnName = "id", insertable = false, updatable = false)
-    var orderItems: List<OrderItem>? = null;
+    var orderItems: List<OrderItem>? = null
 
-    class Order(){}
+    //class Order(){}
 
     /*@ManyToMany
     @JoinTable(
